6f7a03c1e7a9686b725be4d5424e055aad7c23f5
Aaron Kerti <akerti127@gmail.com>
2020-10-18 12:33:00 +0900

    added mongodb.

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-var express = require("express");
-var path = require("path");
-var app = express();
-var port = 6001;
+const express = require('express');
+const path = require('path');
+const mongoClient = require('mongodb').MongoClient;
+const app = express();
+const port = 6001;
+const connStr = 'mongodb://127.0.0.1:27017';
+const dbName = 'budgeteer';
 
 app.get("/", (request, response, next) => {
     response.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
-app.get("/data", (request, response, next) => {
-    response.json(["placeholder"]);
+app.get("/transactions", (request, response, next) => {
+    mongoClient.connect(connStr, function(err, client) {
+        if (err != null) {
+            console.log(err.message);
+        }
+        else {
+            const db = client.db(dbName);
+            const collection = db.collection('transactions');
+
+            collection.find({}).toArray(function(err1, docs) {
+                client.close();
+
+                if (err != null) {
+                    console.log(err1.message);
+                }
+                else {
+                    response.json(docs);
+                }
+            });
+        }
+    });
 });
 
 app.listen(port, ()=>{
